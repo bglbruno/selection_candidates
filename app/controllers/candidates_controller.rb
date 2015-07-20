658359773491504b8ee8bcfb330964a11ab1e5f7
Bruno Gonçales <bgl.bruno@gmail.com>
2015-07-20 14:40:52 -0300

    update show view

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,5 @@
 class CandidatesController < ApplicationController
+  before_action :set_candidate, only: [:show]
 
   def index
     @candidate = Candidate.new
@@ -22,5 +23,10 @@ class CandidatesController < ApplicationController
     def candidate_params
       params.require(:candidate).permit(:name, :email, knowledge_assessment_attributes: [:html, :css, :javascript, :django, :python, :dev_ios, :dev_android])
     end
+
+    def set_candidate
+      @candidate = Candidate.find(params[:id])
+    end
+
 
 end
